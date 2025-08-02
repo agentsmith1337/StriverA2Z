1afaed7185c0226949de061808cf97521b51cf0f
agentsmith1337 <abbhinavbharadwaj@gmail.com>
2025-08-02 21:12:25 +0530

    solved more stars

--- patterns/StarPatterns.java
+++ patterns/StarPatterns.java
@@ -61,5 +61,65 @@ public class StarPatterns {
             System.out.println();
         }
         System.out.println();
+
+        for (int i=0; i<5; i++) {
+            // int j=0; 
+            for (int j=0; j<i; j++) {
+                System.out.print(" ");
+            }
+            for (int j=i; j<5; j++) {
+                System.out.print("*");
+            }
+            for (int j=i; j<4; j++) {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0; i<10; i++) {
+            int j=0;
+            for (j=i; j<4&&i<5; j++ ) System.out.print(" ");
+            for (j=0; j<i+1&&i<5; j++ ) System.out.print("*");
+            for (j=0; j<i&&i<5; j++) System.out.print("*");
+            for (j=0; j<i%5&&i>4; j++) System.out.print(" ");
+            for (j=i%5; j<5&&i>4; j++) System.out.print("*");
+            for (j=i%5; j<4&&i>4; j++) System.out.print("*");
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0; i<=10; i++) {
+            for (int j=i; j<5&&i<5; j++ ) System.out.print(" ");
+            for (int j=0; j<i+1&&i<5; j++ ) System.out.print("*");
+            for (int j=0; j<i&&i<5; j++) System.out.print("*");
+            for (int j=0; j<2*i+1&&i==5; j++) System.out.print("*");
+            for (int j=0; j<(i-1)%5+1&&i>5; j++) System.out.print(" ");
+            for (int j=(i-1)%5; j<5&&i>5; j++) System.out.print("*");
+            for (int j=(i-1)%5; j<4&&i>5; j++) System.out.print("*");
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0; i<9; i++ ) {
+            for (int j=0; j<i+1&&i<5; j++) System.out.print("*");
+            for (int j=(i%5)+1; j<5&&i>4; j++) System.out.print("*");
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=0; i<5; i++) {
+            for (int j=0, b=1; j<i+1; j++, b^=1) System.out.print(b+" ");
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i=1; i<=4; i++) {
+            for (int j=1; j<=i; j++) System.out.print(j);
+            for (int j=i; j<4; j++) System.out.print("  ");
+            for (int j=i; j>=1; j--) System.out.print(j);
+            System.out.println();
+        }
+        System.out.println();
     }
 }
